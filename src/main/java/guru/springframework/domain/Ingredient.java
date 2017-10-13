b0891f4a4c2c170f3a0e92dd788e777002b9acab
DILIP MUDIREDDY <dilip.mudireddy@emc.com>
2017-10-13 18:56:05 +0530

    Correcting typo with Ingredient.

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
-public class Ingrediant {
+public class Ingredient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
